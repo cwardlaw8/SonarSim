998f998e0f62a57ee718b0beb4a088a3a55d8917
Demircan Tas <tasd@mit.edu>
2025-12-07 22:01:57 -0500

    modified for diagonal hydrophone array placement

--- eval_g_Sonar.py
+++ eval_g_Sonar.py
@@ -3,23 +3,30 @@ import numpy as np
 def eval_g_Sonar(x, p, u=None):
     """
     Output: pressure at hydrophone array
-    Pressure is now in x[N:2N] (second half)
     """
     N = p['Nx'] * p['Nz']
     pressure = x[N:2*N].reshape(p['Nx'], p['Nz'])  # CHANGED: was x[:N]
     
     hydrophone_signals = []
     
+    # Horizontal array (shared z, multiple x)
     if 'z_pos' in p['hydrophones'] and 'x_indices' in p['hydrophones']:
         z_pos = p['hydrophones']['z_pos']
         for x_idx in p['hydrophones']['x_indices']:
             if x_idx < p['Nx']:
                 hydrophone_signals.append(pressure[x_idx, z_pos])
     
+    # Vertical array (shared x, multiple z)
     elif 'x_pos' in p['hydrophones'] and 'z_indices' in p['hydrophones']:
         x_pos = p['hydrophones']['x_pos']
         for z_idx in p['hydrophones']['z_indices']:
             if z_idx < p['Nz']:
                 hydrophone_signals.append(pressure[x_pos, z_idx])
+    
+    # Arbitrary paired coordinates
+    elif 'x_indices' in p['hydrophones'] and 'z_indices' in p['hydrophones']:
+        for x_idx, z_idx in zip(p['hydrophones']['x_indices'], p['hydrophones']['z_indices']):
+            if x_idx < p['Nx'] and z_idx < p['Nz']:
+                hydrophone_signals.append(pressure[x_idx, z_idx])
     
     return np.array(hydrophone_signals).reshape(-1, 1)
